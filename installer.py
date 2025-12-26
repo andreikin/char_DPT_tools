888dc1fbf7e838febf2638bf2836fe9e72b7ff0a
Andrey Belyaev <andreikin@mail.ru>
2025-12-26 16:29:46 +0300

    feat: add help

--- installer.py
+++ installer.py
@@ -4,7 +4,10 @@ import json
 import os
 import re
 import shutil
+import sys
+
 import maya.cmds as cmds
+import maya.OpenMaya as om
 
 SHELF_NAME = "Char_DPT_tools"
 COMMAND_FILE_NAME = 'command'
@@ -30,8 +33,6 @@ def install_Char_DPT_tools(distrib_path=None):
     if not distrib_path:
         distrib_path = os.path.dirname(__file__)
     maya_version = cmds.about(version=True)
-    pattern_shelves = r'.+' + maya_version + '/prefs/shelves'
-    shelves_path = [x for x in os.environ['MAYA_SCRIPT_PATH'].split(';') if re.match(pattern_shelves, x)][0]
 
     pattern_scripts = r'.+' + maya_version + '/prefs/scripts'
     scripts_path = [x for x in os.environ['MAYA_SCRIPT_PATH'].split(';') if re.match(pattern_scripts, x)][0]
@@ -50,18 +51,17 @@ def install_Char_DPT_tools(distrib_path=None):
     tools_folders = os.listdir(os.path.join(distrib_path, TOOLS_FOLDER_NAME))
     for tool_folder in tools_folders:
         tool_path = os.path.join(distrib_path, TOOLS_FOLDER_NAME, tool_folder)
-        add_buttons(tool_folder_path=tool_path, scripts_path = scripts_path, icon_path=icon_path)
+        add_button(tool_folder_path=tool_path, scripts_path = scripts_path, icon_path=icon_path)
+
+    om.MGlobal.displayInfo("The shelf " + SHELF_NAME + " has been successfully created!")
 
 
-def add_buttons(tool_folder_path=None, scripts_path=None, icon_path=None):
-    pass
+def add_button(tool_folder_path=None, scripts_path=None, icon_path=None):
     """
         Description of the json file with button creation settings:
-            "label": label for label display mode
-            "imageOverlayLabel": over button text
+            "imageOverlayLabel": label button text
             "command": command that runs the code (if there is a file 'command.py', it is loaded)
             "annotation": "pop-up text with annotation",
-            "image": icon type (if there is a file 'icon.png', it is loaded)
             "sourceType": "python",
             "scripts_list":  list of scripts that need to be copied to the Maya scripts folder 
     """
@@ -76,8 +76,7 @@ def add_buttons(tool_folder_path=None, scripts_path=None, icon_path=None):
         return
 
     #  -----------------------  Loading button settings from json file
-    with open(json_file, 'r') as file:
-        json_data = json.load(file)
+    json_data = load_json_safe(json_file)
 
     #  -----------------------  Copy the script files to the appropriate folder
     scripts_list = json_data.pop('scripts_list') if json_data.get('scripts_list') else []
@@ -89,10 +88,10 @@ def add_buttons(tool_folder_path=None, scripts_path=None, icon_path=None):
 
     #   -----------------------  Create general settings for the button
     data = {'parent': SHELF_NAME, 'overlayLabelColor': [1, 1, 1], 'overlayLabelBackColor': [0.268, 0.268, 0.268, 1]}
+    data.update(json_data)
 
-    for k, v in json_data.items():
-        val = v.encode('utf-8') if isinstance(v, str) else v
-        data[k.encode('utf-8')] = val
+    if not data.get('imageOverlayLabel'):
+        data['imageOverlayLabel'] = ''
 
     #  -----------------------  add command
     for ext in ('.py', '.mel'):
@@ -123,11 +122,44 @@ def add_buttons(tool_folder_path=None, scripts_path=None, icon_path=None):
         dest_icon_path = os.path.join(icon_path, dest_icon_name)
         shutil.copy2(source_icon_file, dest_icon_path)
         data["image"] = dest_icon_path
+    else:
+        data["image"] ='commandButton.png'
 
     #  -----------------------  add button
+    data = convert_bytes_to_str(data) if sys.version[0] == '3' else data
     cmds.shelfButton(**data)
 
 
+def convert_bytes_to_str(data):
+    result = {}
+    for key, value in data.items():
+        key = key.decode('utf-8', errors='ignore') if isinstance(key, bytes) else key
+        result[key] = value.decode('utf-8', errors='ignore') if isinstance(value, bytes) else value
+    return result
+
+
+def load_json_safe(path):
+    """
+    Decoding Russian text in a json file
+    """
+    if sys.version[0] == '3':
+        try:
+            with open(path, 'r', encoding='utf-8-sig') as f:
+                return json.load(f)
+        except UnicodeDecodeError:
+            with open(path, 'r', encoding='cp1251') as f:
+                return json.load(f)
+    else:
+        with open(path, 'r') as f:
+            json_data = json.load(f)
+            out = dict()
+            for k, v in json_data.items():
+                val = v.encode('utf-8') if isinstance(v, str) else v
+                out[k.encode('utf-8')] = val
+            return out
+
+
 if __name__ == '__main__':
-    path = 'U:\AssetStorage\CharTools\char_dpt_tools'
+    path = 'U:\AssetStorage\CharTools\char_dpt_shelf'
+    #path = 'D:\Projects\Python\char_dpt_tools'
     install_Char_DPT_tools(path)
